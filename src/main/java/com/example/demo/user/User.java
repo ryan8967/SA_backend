b75793cf27e6c10b8b2e5321366cdbe2e9f808cf
ryan <ryanjamesyang@gmail.com>
2024-12-12 15:06:42 +0800

    initial commit

---
+++ src/main/java/com/example/demo/user/User.java
@@ -0,0 +1,47 @@
+package com.example.demo.user;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String userName;
+    private String password;
+    private String currPet;
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return userName;
+    }
+
+    public void setName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String currPet() {
+        return currPet;
+    }
+
+    public void currPet(String currPet) {
+        this.currPet = currPet;
+    }
+}
